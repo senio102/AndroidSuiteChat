4e8b1ad4fd54c0d9872e607470d479580bba480d
senio102 <senio102@mail.chapman.edu>
2016-12-12 01:47:52 -0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -12,95 +12,99 @@ import java.net.*;
 //james senior
 public class Server
 {
-	// Maintain list of all client sockets for broadcast
+	// Maintain list of all client sockets
 	private ArrayList<Socket> socketList;
-    public Server()
+	public Server()
 	{
+		//new socketlist to keep track of seperate instances
 		socketList = new ArrayList<Socket>();
 	}
-
 	private void getConnection()
 	{
 		// Wait for a connection from the client
 		try
 		{
-       			InetAddress iAddress = InetAddress.getLocalHost();
-      			System.out.println(iAddress);
+			//print current ip for testing
+			InetAddress iAddress = InetAddress.getLocalHost();
+			System.out.println(iAddress);
 			ServerSocket serverSock = new ServerSocket(4252);
-			// This is an infinite loop, the user will have to shut it down
-			// using control-c
 			int i=0;
 			while (true)
 			{
 				i++;
-				// Send to ClientHandler the socket and arraylist of all sockets
+				//only is called when there are 2 connections, otherwise keeps looping
 				if(i>1){
+					//accepts after 2 succesful connections are maintained
 					Socket connectionSock = serverSock.accept();
 					// Add this socket to the list
 					socketList.add(connectionSock);
+					//pass socketlist to client instance so it has the ability to determine who to send to
 					if(socketList.size()>1)
 						for(Socket b : socketList)
 							new serverThread(b, socketList).start();
 				}
 			}
-			// Will never get here, but if the above loop is given
-			// an exit condition then we'll go ahead and close the socket
-			//serverSock.close();
 		}
 		catch (IOException e)
 		{
 			System.out.println(e.getMessage());
 		}
 	}
-
+	//class to hold the connections
+	class serverThread extends Thread {
+		Socket socket = null;
+		ArrayList<Socket> socketList;
+		//turn variable for tic tac toe
+		int turn = 0;
+		//store current socket on thread to maintain data
+		public serverThread(Socket socket, ArrayList<Socket> i) {
+			this.socket = socket;
+			socketList=i;
+		}
+		//begin communication with specific connection
+		public void run() {
+			try {
+				//print connections 
+				System.out.println(socketList.size());
+				//declare out/in
+				Server j;
+				PrintWriter out= new PrintWriter(socketList.get(1).getOutputStream(), true);
+				//declare out to opposite client (avoid echoing), also assign turns for tic tac toe
+				if(socket == socketList.get(0)){
+					out = new PrintWriter(socketList.get(1).getOutputStream(), true);
+					turn = 1;
+				}
+				if(socket == socketList.get(1)){
+					out = new PrintWriter(socketList.get(0).getOutputStream(), true);
+					turn = 2;
+				}
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				//determine whether there is any input from client
+				String temp = in.readLine();
+				//if input not empty, print
+				while (temp.isEmpty()==false) {
+					//client sends "tictac" whenver game is prompted
+					if(temp.contains("tictac"))
+					{
+						//print turn number to client
+						out.println("turn"+turn);
+					}
+					else
+					{
+						out.println(temp);
+						//refresh string to see if loop should continue
+					}
+					temp = in.readLine();
+				}
+			} catch (Exception e) {
+				//e.printStackTrace();
+			}
+		}
+	}
+	//main
 	public static void main(String[] args)
 	{
 		Server server = new Server();
 		server.getConnection();
 	}
-	class serverThread extends Thread {
-	    Socket socket = null;
-		ArrayList<Socket> socketList;
-		int turn = 0;
-	    //store current socket on thread to maintain data
-	    public serverThread(Socket socket, ArrayList<Socket> i) {
-	        this.socket = socket;
-			socketList=i;
-	    }
-	    //begin communication with specific connection
-	    public void run() {
-	    try {
-			System.out.println(socketList.size());
-	        //declare out/in
-	    	Server j;
-	    	PrintWriter out= new PrintWriter(socketList.get(1).getOutputStream(), true);
-	    	if(socket == socketList.get(0)){
-	    		out = new PrintWriter(socketList.get(1).getOutputStream(), true);
-	    		turn = 1;
-	    	}
-	    	if(socket == socketList.get(1)){
-	    		out = new PrintWriter(socketList.get(0).getOutputStream(), true);
-	    		turn = 2;
-	    	}
-	        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	        //determine whether there is any input from client
-	        String temp = in.readLine();
-	        //if input not empty, print
-	        while (temp.isEmpty()==false) {
-	        	if(temp.contains("tictac"))
-	        	{
-	        		out.println("turn"+turn);
-	        	}
-	        	else
-	        	{
-	            //echo input back to client in all caps for testing
-	            out.println(temp);
-	            //refresh string to see if loop should continue
-	        	}
-	            temp = in.readLine();
-	        }
-	    } catch (Exception e) {
-	        //e.printStackTrace();
-	    }
-	}}
 }
